5bcacecd68d63f016816ef71c7f3d58abf56dac4
Clément Delafargue <divarvel@gmail.com>
2011-02-04 19:19:54 +0100

    Change: I merged everything in the same file, due to issues with the import statement.
    I'll split it up asap

--- GraphToolbox.py
+++ GraphToolbox.py
@@ -2,8 +2,19 @@
 #-*- encoding: utf-8 -*-
 #
 
-import sys, os, json, Graph
+import sys, os, json
 from optparse import OptionParser
+class Graph(object):
+    """ A graph object, and the algos we can apply on it """
+    def __init__(self):
+        pass
+    def load_data(self, data):
+        " OOo style: load Data "
+        self.graph = data
+
+    def shortest_path(self, start, end):
+        " Find the shortest path between two nodes "
+        pass
 
 
 class GraphToolbox(object):
@@ -12,24 +23,24 @@ class GraphToolbox(object):
         " Parse the commands provided by the user"
 
         parser = OptionParser()
-        parser.add_option("-f", "--file", dest="filename",
-                                            help="use the graph stored in FILE",
-                          metavar="FILE")
-
-        parser.add_option("-a", "--action", dest="action",
-                                            help="action to perform on the graph")
-
         (options, args) = parser.parse_args()
 
-        self._load_graph(options['filename'])
+        #The first arg is the input file
+        self._load_graph(args[0])
+
+        if len(args) > 1:
+            action = args[1]
+            if action == "path":
+                if len(args) >=3:
+                    # Shortest path between 2 nodes
+                    print "Shortest path between %s and %s" % (args[2], args[3])
+                    path = self.graph.shortest_path(args[2], args[3])
 
     def _load_graph(self, path):
         " Create a new graph from the input file "
-        graph = new Graph()
-        with(fp = open(path)):
-            graph.loadData(json.load(fp))
-
-        return graph
+        self.graph = Graph()
+        with open(path) as fp:
+            self.graph.load_data(json.load(fp))
 
 if __name__ == '__main__':
-    main = new GraphToolbox()
+    main = GraphToolbox()
