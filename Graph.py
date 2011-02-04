b831fb88f6eb31848bace4d06c98dfd75465081a
Sylvain Joubert <sylvain.joubert@eleves.ec-nantes.fr>
2011-02-04 20:38:51 +0100

    Fix: split classes in multiple files

---
+++ Graph.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+#-*- encoding: utf-8 -*-
+#
+
+class Graph(object):
+    """ A graph object, and the algos we can apply on it """
+
+    def __init__(self):
+        pass
+
+    def load_data(self, data):
+        " OOo style: load Data "
+        self.graph = data
+
+    def shortest_path(self, start, end):
+        """Find the shortest path between nodes start and end
+		Returns [start, node1, node2, ..., end]"""
+        pass
+
+    def max_flow(self, start, end):
+        """Compute the maximum flow between nodes start and end
+		Returns maximum_flow"""
+        pass
+
+    def max_flow_min_cost(self, start, end):
+        """Compute the maximum flow with minimum cost between nodes start and end
+		Returns (maximum_flow, minimum_cost)"""
+        pass
+
+    def transitive_closure(self):
+        " Update the graph to be its transitive closure "
+        pass
+
+	def k_coloring(self, n):
+		" Color the graph with n color "
+		pass
--- GraphToolbox.py
+++ GraphToolbox.py
@@ -2,41 +2,9 @@
 #-*- encoding: utf-8 -*-
 #
 
-import sys, os, json
+import json
 from optparse import OptionParser
-
-class Graph(object):
-    """ A graph object, and the algos we can apply on it """
-
-    def __init__(self):
-        pass
-
-    def load_data(self, data):
-        " OOo style: load Data "
-        self.graph = data
-
-    def shortest_path(self, start, end):
-        """Find the shortest path between nodes start and end
-		Returns [start, node1, node2, ..., end]"""
-        pass
-
-    def max_flow(self, start, end):
-        """Compute the maximum flow between nodes start and end
-		Returns maximum_flow"""
-        pass
-
-    def max_flow_min_cost(self, start, end):
-        """Compute the maximum flow with minimum cost between nodes start and end
-		Returns (maximum_flow, minimum_cost)"""
-        pass
-
-    def transitive_closure(self):
-        " Update the graph to be its transitive closure "
-        pass
-
-	def k_coloring(self, n):
-		" Color the graph with n color "
-		pass
+from Graph import Graph
 
 class GraphToolbox(object):
     """Implement some well known graph-related algos"""
