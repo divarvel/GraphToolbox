23802a44ebf9126351bb61b0a8effe94d2ec5a14
Clément Delafargue <divarvel@gmail.com>
2011-02-04 15:55:12 +0100

    Add: Load a JSON file, try to parse CLI args

--- GraphToolbox.py
+++ GraphToolbox.py
@@ -2,13 +2,34 @@
 #-*- encoding: utf-8 -*-
 #
 
-import sys, os
+import sys, os, json, Graph
+from optparse import OptionParser
+
 
 class GraphToolbox(object):
     """Implement some well known graph-related algos"""
     def __init__(self):
-        " Load the provided graph "
-        pass
+        " Parse the commands provided by the user"
+
+        parser = OptionParser()
+        parser.add_option("-f", "--file", dest="filename",
+                                            help="use the graph stored in FILE",
+                          metavar="FILE")
+
+        parser.add_option("-a", "--action", dest="action",
+                                            help="action to perform on the graph")
+
+        (options, args) = parser.parse_args()
+
+        self._load_graph(options['filename'])
+
+    def _load_graph(self, path):
+        " Create a new graph from the input file "
+        graph = new Graph()
+        with(fp = open(path)):
+            graph.loadData(json.load(fp))
+
+        return graph
 
 if __name__ == '__main__':
-    main = GraphToolbox()
+    main = new GraphToolbox()
