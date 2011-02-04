88cd5ef4a42089e4583674ba950cdcd20d14e25e
Clément Delafargue <divarvel@gmail.com>
2011-02-04 15:28:09 +0100

    Add: Beginning

---
+++ Graph.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#-*- encoding: utf-8 -*-
+#
+
+
+class Graph(object):
+    """ A graph object, and the algos we can apply on it """
+    def shortest_path(self, start=a, end=b):
+        " Find the shortest path between two nodes "
+        pass
+
+
---
+++ GraphToolbox.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#-*- encoding: utf-8 -*-
+#
+
+import sys, os
+
+class GraphToolbox(object):
+    """Implement some well known graph-related algos"""
+    def __init__(self):
+        " Load the provided graph "
+        pass
+
+if __name__ == '__main__':
+    main = GraphToolbox()
